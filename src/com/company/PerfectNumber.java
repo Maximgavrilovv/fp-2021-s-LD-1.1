0c84bf381943d74712a1a332b8c113057e818e01
maxtamme <maxtamme@gmail.com>
2021-09-16 18:57:11 +0300

    commit

---
+++ src/com/company/PerfectNumber.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.Set;
+import java.util.HashSet;
+
+public class PerfectNumber {
+
+    protected enum State {
+        DEFICIENT, PERFECT, ABUNDANT
+    }
+
+    public static Set<Integer> Divisors(int number) {
+
+        Set<Integer> divisors = new HashSet<>();
+        for (int i = 1;i <= number; i++) {
+            if (number % i == 0) {
+                divisors.add(i);
+            }
+        }
+
+        return divisors;
+    }
+
+    public static State Process(int n) {
+
+        int sum = Divisors(n).stream().mapToInt(Integer::intValue).sum();
+        sum = sum / 2;
+
+        if (sum == n)
+        {
+            return State.PERFECT;
+        }
+        else if(sum < n)
+        {
+            return State.DEFICIENT;
+        }
+        else
+        {
+            return State.ABUNDANT;
+        }
+    }
+}
